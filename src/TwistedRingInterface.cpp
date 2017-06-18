45cd240b98fb4a84d49e25345c1bf4025ac9908c
吉田貴寿 <tyoshida@tyoshidaMBP.lan>
2017-06-18 12:15:38 +0900

    2nd commit

---
+++ src/TwistedRingInterface.cpp
@@ -0,0 +1,22 @@
+//
+//  TwistedRingInterface.cpp
+//  iTrain-LSD
+//
+//  Created by 吉田貴寿 on 2017/06/18.
+//
+//
+
+#include "TwistedRingInterface.hpp"
+
+
+
+
+TwistedRingInterface::TwistedRingInterface(){
+    
+}
+
+
+TwistedRingInterface::~TwistedRingInterface(){
+    
+}
+
---
+++ src/TwistedRingInterface.hpp
@@ -0,0 +1,31 @@
+//
+//  TwistedRingInterface.hpp
+//  iTrain-LSD
+//
+//  Created by 吉田貴寿 on 2017/06/18.
+//
+//
+
+#ifndef TwistedRingInterface_hpp
+#define TwistedRingInterface_hpp
+
+#include <stdio.h>
+#include "ofMain.h"
+
+class TwistedRingInterface{
+
+    ofSerial serial;
+    
+    TwistedRingInterface();
+    ~TwistedRingInterface();
+    
+    
+    
+    
+    
+    
+    
+};
+
+
+#endif /* TwistedRingInterface_hpp */
