63b4cad4cde0aed64ee2ee920db4d049fd0b70c4
Hayato Nishioka <tapioka.euler@gmail.com>
2017-06-30 06:04:58 +0900

    read 32 bytes

--- arduino/readLED/readLED.ino
+++ arduino/readLED/readLED.ino
@@ -1,18 +1,18 @@
 #include <Wire.h>
- 
+
 #define LDaddrs1 (0x70)  //LEDドライバーHT16K33　アドレス
- 
+
 byte LedDot[8] = {
-                  B10000001,
-                  B11000001,
-                  B10100001,
-                  B10010001,
-                  B10001001,
-                  B10000101,
-                  B10000011,
-                  B10000001
-                  };
- 
+  B10000001,
+  B11000001,
+  B10100001,
+  B10010001,
+  B10001001,
+  B10000101,
+  B10000011,
+  B10000001
+};
+
 byte LedDot2[8];
 
 byte LedDots[8][8];
@@ -22,15 +22,17 @@ byte LedDots2[8][8];
 int nownum;
 int prenum;
 
- 
+bool flag_serial = false;
+
+
 //***********セットアップ***************************************************
-void setup() 
+void setup()
 {
   Serial.begin(115200);
   Wire.begin(); // initialise the connection
   Wire.setClock(400000L); //HT16K33のクロックはMax 400kHz
   int i;
-  for(i = 0; i < 8; i++){
+  for (i = 0; i < 8; i++) {
     LED_Driver_Setup( LDaddrs1 + i, 1); //HT16K33システムオシレータ　ＯＮ
     LED_Driver_Blink( LDaddrs1 + i, 1, 0); //blink_Hz=0 点滅off, 1は2Hz, 2は1Hz, 3は0.5Hz, on_off=0は消灯、1は点灯
     LED_Driver_Brightness( LDaddrs1 + i, 1 ); // brightness= 0～15
@@ -40,24 +42,34 @@ void setup()
 
   prenum = 0;
   nownum = 1;
-  
-  pinMode(13,OUTPUT);
+
+  flag_serial = true;
+
+  pinMode(13, OUTPUT);
 }
 //**************メインループ*************************************************
 
 void loop() {
-  
-  if(Serial.available() >= 63){
-      digitalWrite(13,HIGH);//debug
 
-    for(int i = 0; i < 8; i++){
-      for(int j = 0; j < 8; j++){
-         LedDots[i][j] = Serial.read();
-      //        LedDots[i][j] = 0xAA;
+  if (Serial.available() >= 32) {
+    digitalWrite(13, HIGH); //debug
 
+    if (flag_serial) {
+      for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 8; j++) {
+          LedDots[i][j] = Serial.read();
+        }
+        LED_Ada88_cnv(LedDots[i], LedDots2[i]);
+        LED_Driver_DisplayOutput(LDaddrs1 + i, LedDots2[i]);
       }
-      LED_Ada88_cnv(LedDots[i], LedDots2[i]);
-      LED_Driver_DisplayOutput(LDaddrs1 + i, LedDots2[i]);
+    } else {
+      for (int i = 4; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+          LedDots[i][j] = Serial.read();
+        }
+        LED_Ada88_cnv(LedDots[i], LedDots2[i]);
+        LED_Driver_DisplayOutput(LDaddrs1 + i, LedDots2[i]);
+      }
     }
   }
 }
@@ -75,7 +87,7 @@ void LED_Driver_Blink(byte LD_addrs, byte on_off, byte blink_Hz)
 {
   //blink_Hz=0 点滅off, 1は2Hz, 2は1Hz, 3は0.5Hz, on_off=0は消灯、1は点灯　
   Wire.beginTransmission(LD_addrs);
-  Wire.write(0x80 | (blink_Hz<<1) | on_off); 
+  Wire.write(0x80 | (blink_Hz << 1) | on_off);
   Wire.endTransmission();
 }
 //**********************ＬＥＤドライバ　HT16K33　明るさ設定*********************
@@ -91,31 +103,31 @@ void LED_Driver_DisplayInt(byte LD_addrs)
 {
   Wire.beginTransmission(LD_addrs);
   Wire.write(0x00);
-  for(int i=0;i<8;i++){  
-        Wire.write(B00000000);                   //1つ目の8x8LED初期化
-        Wire.write(B00000000);                   //2つ目の8x8LED初期化
+  for (int i = 0; i < 8; i++) {
+    Wire.write(B00000000);                   //1つ目の8x8LED初期化
+    Wire.write(B00000000);                   //2つ目の8x8LED初期化
   }
   Wire.endTransmission();
 }
 //**********************Adafruit8x8ドット bit変換******************************
 void LED_Ada88_cnv(byte* Bdot1, byte* Bdot2)
 {
-  for(byte i=0; i<8; i++){
-    for(byte j=0; j<7; j++){
-     bitWrite(Bdot2[i],6-j,bitRead(Bdot1[i],j));
+  for (byte i = 0; i < 8; i++) {
+    for (byte j = 0; j < 7; j++) {
+      bitWrite(Bdot2[i], 6 - j, bitRead(Bdot1[i], j));
     }
-    bitWrite( Bdot2[i],7,bitRead(Bdot1[i],7));  
+    bitWrite( Bdot2[i], 7, bitRead(Bdot1[i], 7));
   }
 }
 //**********************ＬＥＤドライバ　HT16K33　8X8データ送信*******************
 void LED_Driver_DisplayOutput(byte LD_addrs, byte* DotB)
 {
-  int i,j;
-        Wire.beginTransmission(LD_addrs);
-        Wire.write(B00000000); //これは必要
-        for(i = 0; i<8; i++){
-          Wire.write(DotB[i]);
-          Wire.write(0); //2つ目のLEDがある場合はここで送るが、今回は無し
-        }
-        Wire.endTransmission();
+  int i, j;
+  Wire.beginTransmission(LD_addrs);
+  Wire.write(B00000000); //これは必要
+  for (i = 0; i < 8; i++) {
+    Wire.write(DotB[i]);
+    Wire.write(0); //2つ目のLEDがある場合はここで送るが、今回は無し
+  }
+  Wire.endTransmission();
 }
