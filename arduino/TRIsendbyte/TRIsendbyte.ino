26e43a9e7d2ce346906bd62f629812321c246ab0
Hayato Nishioka <tapioka.euler@gmail.com>
2017-07-05 06:13:16 +0900

    分離した

--- arduino/TRIsendbyte/TRIsendbyte.ino
+++ arduino/TRIsendbyte/TRIsendbyte.ino
@@ -29,9 +29,10 @@ void loop() {
 
   // put your main code here, to run repeaedly:  
   int nowpos0 = analogRead(A0) - defaultValue0;
-  if (nowpos0 > 31) nowpos0 = 31;
-  if (nowpos0 < -31) nowpos0 = -31;
+  if (nowpos0 > 127) nowpos0 = 127;
+  if (nowpos0 < -127) nowpos0 = -127;
 
+/*
   //下位6ビットがポテンショの値、右から6ビット目が符号
   if (nowpos0 < 0) {
     sendByte0 = (byte) abs(nowpos0);
@@ -41,11 +42,13 @@ void loop() {
     sendByte0 = (byte) abs(nowpos0);
     sendByte0 = sendByte0 & 0x1F;//下位5bitのみコピー  
   }
+:*/
 
   int nowpos1 = analogRead(A1) - defaultValue1;
-  if (nowpos1 > 31) nowpos1 = 31;
-  if (nowpos1 < -31) nowpos1 = -31;
+  if (nowpos1 > 127) nowpos1 = 127;
+  if (nowpos1 < -127) nowpos1 = -127;
 
+/*
   if (nowpos1 < 0) {
     sendByte1 = (byte) abs(nowpos1);
     sendByte1 = sendByte1 & 0x1F;//下位5bitのみコピー
@@ -54,30 +57,47 @@ void loop() {
     sendByte1 = (byte) abs(nowpos1);
     sendByte1 = sendByte1 & 0x1F;//下位5bitのみコピー    
   }
-
+*/
 
 
   //上から2bit目がスイッチのオンオフ
   //*1/0******
+ char is_on0 = 0;
+ char is_on1 = 0;
   if (digitalRead(3) == HIGH) {
-    sendByte0 = sendByte0 & 0xBF;
+    //sendByte0 = sendByte0 & 0xBF;
+    is_on0 = 1;
     digitalWrite(13, HIGH);
   } else {
-    sendByte0 = sendByte0 | 0x40;
+    //sendByte0 = sendByte0 | 0x40;
     digitalWrite(13, LOW);
+    is_on0 = 0;
   }  
   if (digitalRead(2) == HIGH) {
-    sendByte1 = sendByte1 & 0xBF;
+    //sendByte1 = sendByte1 & 0xBF;
+    is_on1 = 1;
   } else {
-    sendByte1 = sendByte1 | 0x40;
+    //sendByte1 = sendByte1 | 0x40;
+    is_on1 = 0;
   }
 
   //一番上のビットがID(０か１）
-  if (0) sendByte0 = sendByte0 | 0x80;
-  if (1) sendByte1 = sendByte1 | 0x80;
+  //if (0) sendByte0 = sendByte0 | 0x80;
+  //if (1) sendByte1 = sendByte1 | 0x80;
+
+  Serial.write(0xFF);
+  Serial.write(0x00);
+  Serial.write(is_on0);
+  Serial.write((char)nowpos0);
+
+  Serial.write(0xFF);
+  Serial.write(0x01);
+  Serial.write(is_on1);
+  Serial.write((char)nowpos1);
+
   
-  Serial.write(sendByte0);
-  Serial.write(sendByte1);
+  //Serial.write(sendByte0);
+  //Serial.write(sendByte1);
 
   //For Debug
   //Serial.print(sendByte0, BIN);
