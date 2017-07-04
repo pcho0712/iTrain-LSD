97a0f89fcc401d6626f33d863011e16dc4050e70
Hayato Nishioka <tapioka.euler@gmail.com>
2017-07-05 04:07:43 +0900

    Move 2 TRI!!
    多分バグ直った
    Charは2Byte

--- arduino/TRIsendbyte/TRIsendbyte.ino
+++ arduino/TRIsendbyte/TRIsendbyte.ino
@@ -1,54 +1,86 @@
-int defaultValue = 0;
-//byte sendByte = 0x00;
+int defaultValue0 = 0;
+int defaultValue1 = 0;
+
 bool triId = true;
 
 void setup() {
   // put your setup code here, to run once:
   // put your setup code here, to run once:
   Serial.begin(9600);
-  pinMode(12, INPUT);
+  pinMode(2, INPUT_PULLUP);
   pinMode(13, OUTPUT);
-
+  pinMode(3, INPUT_PULLUP);
 
   int i = 0;
-  int sum = 0;
+  defaultValue0 = 0;
+  defaultValue1 = 0;
   for (i = 0; i < 10; i++) {
-    sum += analogRead(A0);
+    defaultValue0 += analogRead(A0);
+    defaultValue1 += analogRead(A1);
   }
-  defaultValue = sum / 10;
+  defaultValue0 = defaultValue0 / 10;
+  defaultValue1 = defaultValue1 / 10;
 
 }
 
 void loop() {
-  char sendByte = 0x00;
+  byte sendByte0 = 0x00;
+  byte sendByte1 = 0x00;
 
-  // put your main code here, to run repeaedly:
-  /*
-  int nowpos = analogRead(A0) - defaultValue;
-  if (nowpos > 31) nowpos = 31;
-  if (nowpos < -31) nowpos = -31;
+  // put your main code here, to run repeaedly:  
+  int nowpos0 = analogRead(A0) - defaultValue0;
+  if (nowpos0 > 31) nowpos0 = 31;
+  if (nowpos0 < -31) nowpos0 = -31;
+
   //下位6ビットがポテンショの値、右から6ビット目が符号
-  sendByte = (char) nowpos;
-  sendByte = sendByte & 0x1F;//下位5bitのみコピー
-  if (nowpos < 0) {
-    sendByte = sendByte | 0x20; //6bit目に符号
+  if (nowpos0 < 0) {
+    sendByte0 = (byte) abs(nowpos0);
+    sendByte0 = sendByte0 & 0x1F;//下位5bitのみコピー
+    sendByte0 = sendByte0 | 0x20; //6bit目に符号
+  }else{
+    sendByte0 = (byte) abs(nowpos0);
+    sendByte0 = sendByte0 & 0x1F;//下位5bitのみコピー  
   }
-  */
+
+  int nowpos1 = analogRead(A1) - defaultValue1;
+  if (nowpos1 > 31) nowpos1 = 31;
+  if (nowpos1 < -31) nowpos1 = -31;
+
+  if (nowpos1 < 0) {
+    sendByte1 = (byte) abs(nowpos1);
+    sendByte1 = sendByte1 & 0x1F;//下位5bitのみコピー
+    sendByte1 = sendByte1 | 0x20; //6bit目に符号
+  }else{
+    sendByte1 = (byte) abs(nowpos1);
+    sendByte1 = sendByte1 & 0x1F;//下位5bitのみコピー    
+  }
+
 
 
   //上から2bit目がスイッチのオンオフ
-  if (digitalRead(12) == HIGH) {
-    sendByte = sendByte & 0xBF;
+  //*1/0******
+  if (digitalRead(3) == HIGH) {
+    sendByte0 = sendByte0 & 0xBF;
     digitalWrite(13, HIGH);
   } else {
-    sendByte = sendByte | 0x40;
+    sendByte0 = sendByte0 | 0x40;
     digitalWrite(13, LOW);
+  }  
+  if (digitalRead(2) == HIGH) {
+    sendByte1 = sendByte1 & 0xBF;
+  } else {
+    sendByte1 = sendByte1 | 0x40;
   }
 
   //一番上のビットがID(０か１）
-  //  bitWrite(sendByte, 7, triID);
-  if (triId) sendByte = sendByte | 0x80;
+  if (0) sendByte0 = sendByte0 | 0x80;
+  if (1) sendByte1 = sendByte1 | 0x80;
+  
+  Serial.write(sendByte0);
+  Serial.write(sendByte1);
 
-  Serial.write(sendByte);
-  //Serial.write(0xAA);
+  //For Debug
+  //Serial.print(sendByte0, BIN);
+  //Serial.print(" ");
+  //Serial.println(sendByte1, BIN);
 }
